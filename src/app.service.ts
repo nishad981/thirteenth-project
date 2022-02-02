262164a16b6555c16391b62b20ab2c3cfb4f5c82
Nishad Bijukchhe <dahsin981@gmail.com>
2022-02-02 16:52:37 +0545

    CRUD on dummy entity

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @InjectRepository(User) private usersRepository: Repository<User>,
+  ) {}
+
+  getAll(): Promise<User[]> {
+    return this.usersRepository.find(); //SELECT * from User
+  }
+
+  async getOneById(id: number): Promise<User> {
+    try {
+      const user = await this.usersRepository.findOneOrFail(id); //SELECT * from User WHERE user.id=id;
+      return user;
+    } catch (error) {
+      //handle error
+      throw error;
+    }
+  }
+
+  createUser(name: string): Promise<User> {
+    const newUser = this.usersRepository.create({ name }); //const newUser = new User(); / newUser.name = name;
+
+    return this.usersRepository.save(newUser);
+  }
+
+  async updateUser(id: number, name: string): Promise<User> {
+    const user = await this.getOneById(id);
+    user.name = name;
+    return this.usersRepository.save(user); // Update
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
